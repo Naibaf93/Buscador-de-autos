56b986bbffd83cfe33b6b7bcb9e4a3a73ab599ab
Fabián Huertas <miloh9318@hotmail.com>
2024-03-13 19:05:26 -0500

    generando un select de años

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 // Variables
 
 const resultado  = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear();
+const min = max - 10;
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();
+    mostrarAutos(); // muestra los auto al cargar
+
+    // llena las opciones de años 
+    llenarSelect();
 })
 
 // Funciones
@@ -24,4 +31,15 @@ function mostrarAutos() {
 
         resultado.appendChild(autoHTML)
     })
+}
+
+// Generar los años del select
+
+function llenarSelect() {
+    for(let i = max; i >= min; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion .textContent = i;
+        year.appendChild(opcion); //Agrega las opciones de año a select
+    }
 }
