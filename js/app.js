abc367c998eab8b8d4c268dbd11b9252724a30b9
Fabián Huertas <miloh9318@hotmail.com>
2024-03-15 15:44:07 -0500

    Filtrar por precio

--- js/app.js
+++ js/app.js
@@ -49,10 +49,14 @@ year.addEventListener('change', e => {
 
 minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value ;
+
+    filtrarAuto();
 });
 
 maximo.addEventListener('change', e => {
     datosBusqueda.maximo = e.target.value ;
+
+    filtrarAuto();
 });
 
 puertas.addEventListener('change', e => {
@@ -111,7 +115,7 @@ function llenarSelect() {
 // Funcion que filtra en base a la busqueda
 
 function filtrarAuto() {
-    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear )
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter( filtrarMinimo ).filter( filtrarMaximo )
     // console.log(resultado);
 
     mostrarAutos(resultado);
@@ -129,6 +133,22 @@ function filtrarYear(auto) {
     const { year } = datosBusqueda;
     if( year ) {
         return auto.year === year;     
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto) {
+    const { minimo } = datosBusqueda;
+    if( minimo ) {
+        return auto.precio >= minimo;     
+    }
+    return auto;
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda;
+    if( maximo ) {
+        return auto.precio <= maximo;     
     }
     return auto;
 }
